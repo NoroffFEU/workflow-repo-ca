d45903b79d6de5560380680b339ff3ed56210a55
Lyn Resell <61920408+lynar13@users.noreply.github.com>
2024-11-27 19:27:44 +0100

    Added playwright testing tool for login and navigation test cases

--- vitest.config.js
+++ vitest.config.js
@@ -4,7 +4,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    include: ['tests/**/*.test.js'], // Files to include for testing
+    include: ['tests/vitest/**/*.test.js'], // Files to include for testing
     coverage: {
       provider: 'c8',
       reporter: ['text', 'html'],
