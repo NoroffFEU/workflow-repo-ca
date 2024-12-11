2f111ed3e94012441746d42f671b97d0623bd790
Kenneth Jahnsen Collins <106404582+kJAHsin@users.noreply.github.com>
2024-12-11 11:27:37 +0100

    comments on isActivePath unit tests

--- js/ui/common/createMenu.test.js
+++ js/ui/common/createMenu.test.js
@@ -6,27 +6,28 @@ import { isActivePath } from '../../utils/userInterface';
 // 3. Returns true when current path includes the href
 // 4. Returns false when paths don't match
 
+// 1
 test('returns true when path matches', () => {
 	const href = '/beans';
 	const currentPath = '/beans';
 
 	expect(isActivePath(href, currentPath)).toBe(true);
 });
-
+// 2
 test('returns true when path and href are both root', () => {
 	const href = '/';
 	const currentPath = '/index.html';
 
 	expect(isActivePath(href, currentPath)).toBe(true);
 });
-
+// 3
 test('returns true when current path includes href', () => {
 	const href = '/path';
 	const currentPath = 'www.website.com/path';
 
 	expect(isActivePath(href, currentPath)).toBe(true);
 });
-
+// 4
 test("returns false when paths don't match", () => {
 	const href = '/beansontoast';
 	const currentPath = 'www.website.com/path';
