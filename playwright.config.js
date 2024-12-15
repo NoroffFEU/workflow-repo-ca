143bee30bacd312908a0bd7be38c5729622ea225
marned91 <152280051+marned91@users.noreply.github.com>
2024-12-15 17:20:35 +0100

    adding login test
    3 test cases: login successful, login failed wrong password, login failed wrong email. Struggling a bit with the tests due to timeout error

--- playwright.config.js
+++ playwright.config.js
@@ -13,21 +13,10 @@ const { defineConfig, devices } = require("@playwright/test");
 
 require("dotenv").config();
 
-export default defineConfig({
-  webServer: {
-    command: "npm run start",
-    url: "http://localhost:5500",
-    reuseExistingServer: !process.env.CI,
-  },
-  use: {
-    baseURL: "http://localhost:5500",
-  },
-});
-
 module.exports = defineConfig({
   testDir: "./tests",
   /* Run tests in files in parallel */
-  fullyParallel: true,
+  fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
@@ -39,7 +28,7 @@ module.exports = defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
+    baseURL: "http://localhost:5500",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
@@ -84,9 +73,9 @@ module.exports = defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: "npm run start",
+    url: "http://localhost:5000",
+    reuseExistingServer: !process.env.CI,
+  },
 });
---
+++ tests/login.test.js
@@ -0,0 +1,49 @@
+import { expect, test } from "@playwright/test";
+
+test.describe("login", () => {
+  //Successful path: User can login with valid userinfo
+  test("user can login", async ({ page }) => {
+    await page.goto("login/index.html");
+
+    await page.locator('input[name="email"]').fill(process.env.TEST_USER_EMAIL);
+    await page
+      .locator('input[name="password"]')
+      .fill(process.env.TEST_USER_PASSWORD);
+
+    await page.getByRole("button", { name: "Login" }).click();
+
+    await expect(page.getByRole("button", { name: "Logout" })).toBeVisible();
+  });
+
+  //Error Path: Wrong password shows error message
+  test("wrong password shows error", async ({ page }) => {
+    await page.goto("/login/index.html");
+
+    await page.locator('input[name="email"]').fill(process.env.TEST_USER_EMAIL);
+    await page.locator('input[name="password"]').fill("wrongpassword");
+
+    await page.getByRole("button", { name: "Login" }).click();
+
+    await expect(page.locator("#message-container")).toContainText(
+      "Invalid email or password",
+    );
+  });
+
+  //Error path: Wrong email shows error message
+  test("wrong email shows error", async ({ page }) => {
+    await page.goto("/login/index.html");
+
+    await page
+      .locator('input[name="email"]')
+      .fill("wrongemailaddress@example.com");
+    await page
+      .locator('input[name="password"]')
+      .fill(process.env.TEST_USER_PASSWORD);
+
+    await page.getByRole("button", { name: "Login" }).click();
+
+    await expect(page.locator("#message-container")).toContainText(
+      "Please enter a noroff.no or stud.noroff.no email address",
+    );
+  });
+});
