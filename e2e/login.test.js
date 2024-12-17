205363a9e860821d1ccb389dc8adaf091035340c
Emily A. Brynestad-Hagen <emily.brynestad@hotmail.com>
2024-12-17 14:18:47 +0100

    refining code for final delivery

--- e2e/login.test.js
+++ e2e/login.test.js
@@ -9,15 +9,15 @@ test.describe("Login flow", () => {
   }) => {
     await page.goto("http://127.0.0.1:5500/login/");
 
-    // Fyll ut skjemaet med gyldige data
+    // Fill out form with correct data
     await page.fill('input[name="email"]', process.env.VITE_LOGIN_USERNAME);
     await page.fill('input[name="password"]', process.env.VITE_LOGIN_PASSWORD);
     await page.click('button[type="submit"]');
 
-    // Verifiser at brukeren blir omdirigert til riktig URL
+    // Verify that the user is redirected to the appropriate URL
     await expect(page).toHaveURL("http://127.0.0.1:5500/");
 
-    // Valider om velkomstmeldingen finnes
+    // Validate if theres a welcome message
     const header = page.locator("h1");
     await expect(header).toHaveText("Welcome to this site");
   });
@@ -27,12 +27,12 @@ test.describe("Login flow", () => {
   }) => {
     await page.goto("/login/");
 
-    // Fyll ut skjemaet med ugyldige data
+    // Fill out the form with invalid data
     await page.fill('input[name="email"]', "invalid@example.com");
     await page.fill('input[name="password"]', "wrongpassword");
     await page.click('button[type="submit"]');
 
-    // Valider feilmeldingen
+    // Validete error message
     const messageContainer = page.locator("#message-container");
     await expect(messageContainer).toBeVisible();
     await expect(messageContainer).toContainText(
