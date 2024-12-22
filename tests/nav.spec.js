c7b79fd7042f02689da08a2310fd82f071c3873b
Kenneth Jahnsen Collins <106404582+kJAHsin@users.noreply.github.com>
2024-12-22 18:45:17 +0100

    comments

--- tests/nav.spec.js
+++ tests/nav.spec.js
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('navigates to venue on click', async ({ page }) => {
-	await page.goto(`/`);
-	await page.locator('.bg-cover').first().click();
-	await expect(page.locator('h1')).toContainText('Venue details');
+	await page.goto(`/`); // waits for load event :)
+	await page.locator('.bg-cover').first().click(); // locator for first venue
+	await expect(page.locator('h1')).toContainText('Venue details'); // heading on page
 });
