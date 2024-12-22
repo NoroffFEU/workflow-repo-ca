3e1e4ad987778f4ad48b3f94981d35f2900195ce
ziconstr <76712091+ziconstr@users.noreply.github.com>
2024-12-22 23:50:30 +0100

    test

--- tests/e2e/homepage.spec.js
+++
@@ -1,6 +0,0 @@
-const { test, expect } = require("@playwright/test");
-
-test("Homepage has the correct title", async ({ page }) => {
-  await page.goto("http://localhost:5500"); // Update URL as needed
-  await expect(page).toHaveTitle(/Your Website Title/); // Update title as needed
-});
